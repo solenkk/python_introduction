70216bf5066cb27dedd0c7aa353f5584dd9cb3e0
solenkk <kassahunsolen@gmail.com>
2025-11-09 23:23:23 +0300

    sj

---
+++ basic_operations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# basic_operations.py
+
+# Assign values to variables
+number1 = 10
+number2 = 5
+
+# Perform arithmetic operations
+addition_result = number1 + number2
+subtraction_result = number1 - number2
+multiplication_result = number1 * number2
+
+# Print the results in the required format
+print(f"Addition of {number1} and {number2} is {addition_result}")
+print(f"Subtraction of {number1} and {number2} is {subtraction_result}")
+print(f"Multiplication of {number1} and {number2} is {multiplication_result}")
